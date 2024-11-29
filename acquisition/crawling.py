87f1b7eb8477cb006c55c908d2f7a5dc4a2e1e49
k-kbk <kkbk0077@gmail.com>
2024-11-30 01:59:04 +0900

    refactor: chrome option 추가

--- acquisition/crawling.py
+++ acquisition/crawling.py
@@ -16,6 +16,8 @@ def setup_download_directory():
 def setup_chrome_options(chrome_options):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless") 
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_experimental_option("prefs", {
         "download.default_directory": DOWNLOAD_DIRECTORY,
         "download.prompt_for_download": False
