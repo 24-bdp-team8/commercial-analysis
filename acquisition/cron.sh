c4d9c8980e56ece016c675a136e38a936affbf79
k-kbk <kkbk0077@gmail.com>
2024-12-07 23:21:24 +0900

    chore: 명령어 수정

--- acquisition/cron.sh
+++ acquisition/cron.sh
@@ -1,2 +1,2 @@
 # 2, 5, 8, 11월의 1일 0시 0분마다 작업 실행
-(crontab -l 2>/dev/null; echo "0 0 1 2,5,8,11 * /home/user/project/scripts/run_crawler.sh") | crontab -
+(crontab -l 2>/dev/null; echo "0 0 1 2,5,8,11 * /opt/anaconda3/bin/python /Users/bk/Documents/code/commercial-analysis/acquisition/crawling.py") | crontab -
